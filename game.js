3c4bbe31425918d7479c74fc68e47afd78df5b5f
NattyNgrn <natnrgn@gmail.com>
2023-08-09 21:33:00 -0400

    create event listener and start function

--- game.js
+++ game.js
@@ -3,3 +3,12 @@ let button = document.getElementById("guessbtn");
 let message = document.getElementById("winorlose");
 let targetNum = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
+
+button.addEventListener("click",() => {
+    let userguess = parseInt(input.value);
+    if(userguess < 1 || userguess > 100){
+        message.textContent = "Please guess a valid number between 1-100."
+        return;
+    }
+    attempts++;
+    
